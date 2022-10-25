0859792f6b5ffa309c354aed14680359d6d18e34
Amara <73879499+40478626@users.noreply.github.com>
2022-10-25 09:21:05 +0630

    Login

--- ExampleProject/src/Main.java
+++ ExampleProject/src/Main.java
@@ -1,6 +1,26 @@
 public class Main {
+
     public static void main(String[] args) {
-       VerifyPassword.verifyPassword();
-       GenerateToken.generateToken();
+
+        VerifyPassword.verifyPassword();
+        GenerateToken.generatetoken();
+        MessageToUser.messageToUser();
+    }
+}
+class VerifyPassword {
+    public static void verifyPassword() {
+        System.out.println("Password Verified");
+    }
+}
+
+class GenerateToken {
+    public static void generatetoken(){
+        System.out.println("Generate Token");
+    }
+}
+
+class MessageToUser {
+    public static void messageToUser(){
+        System.out.println("Password Valid!");
     }
 }
